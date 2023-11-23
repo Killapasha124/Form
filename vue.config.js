ac051b9b3d8ea2be187cea1581efa76577896f72
Pavel <51490982+Killapasha124@users.noreply.github.com>
2023-11-23 15:38:09 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-<<<<<<< HEAD
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/Form/' 
     : '/'
 }
-=======
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/form/'
-    : '/'
-}
->>>>>>> 4554f8ddab70aa488ebc19f960444e3f78adb1ff
+
